016e96597ad8f6333564a562b211a6b7d468bf29
Stratos Gerakakis <stratos@gerakakis.net>
2025-01-29 20:38:49 +0100

    Increase logging visibility

--- entrypoint.py
+++ entrypoint.py
@@ -10,7 +10,7 @@ def compile(filename: str, options: list[str]) -> bool:
     Returns True if the typst command exited with status 0, False otherwise.
     """
     command = ["typst", "compile"] + options + [filename]
-    logging.debug("Running: " + " ".join(command))
+    logging.info("Running: " + " ".join(command))
 
     result = subprocess.run(command, capture_output=True, text=True)
     try:
